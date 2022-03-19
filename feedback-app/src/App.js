7804bfd9bd3f9f14ffae05fb0447dbd1807060f6
princenyamadi <princenyamadi.pn@gmail.com>
2022-03-19 07:44:33 +0000

    react basics and jsx

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const title = 'Blog Post'
+  const body = 'This is my blog post'
+  const comments = [
+    { id: 1, text: 'Comment one' },
+    { id: 2, text: 'Comment two' },
+    { id: 3, text: 'Comment three' },
+  ]
+
+  const loading = false
+  const showComments = true
+  const commnetBlock = (
+    <div>
+      <h3>Comments ({comments.length})</h3>
+      <ul>
+        {comments.map((comment, index) => (
+          <li>{comment.text}</li>
+        ))}
+      </ul>
     </div>
-  );
+  )
+
+  if (loading) return <h1>Loading...</h1>
+  return (
+    <div className="container">
+      <h1>{title}</h1>
+      <p>{body}</p>
+
+      {showComments && commnetBlock}
+    </div>
+  )
 }
 
-export default App;
+export default App
